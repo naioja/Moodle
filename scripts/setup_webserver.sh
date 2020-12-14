f4bf0651f30d872adf8b27bc3900107964884c19
Adrian Joian <ajoian@microsoft.com>
2020-12-14 23:01:03 +0100

    installing azurecli in a screen session

--- scripts/setup_webserver.sh
+++ scripts/setup_webserver.sh
@@ -133,8 +133,8 @@ EOF
     
   fi
 
-  # install azure-cli
-  nohup curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash &
+  # install azure-cli 
+  sudo screen -dmLS installing-async-azure-cli bash -c "curl -sL https://aka.ms/InstallAzureCLIDeb | bash /tmp/InstallAzureCLIDeb"
   wget -O azcopy_v10.tar.gz https://aka.ms/downloadazcopy-v10-linux && tar -xf azcopy_v10.tar.gz --strip-components=1 && sudo cp ./azcopy_linux_amd64_*/azcopy /usr/bin/
    
   # PHP Version
