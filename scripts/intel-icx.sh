1f62defaf33b26cc340df6340ef9ddcdc4550653
Adrian Joian <ajoian@microsoft.com>
2021-05-23 18:32:19 +0200

    Adding required modules for nginx

--- scripts/intel-icx.sh
+++ scripts/intel-icx.sh
@@ -43,6 +43,7 @@ autotools-dev \
 libtool \
 automake \
 zlib1g \
+libgeoip-dev \
 zlib1g-dev \
 nano \
 netcat \
@@ -118,16 +119,17 @@ cd asynch_mode_nginx
   --pid-path=/run/nginx.pid \
   --lock-path=/run/lock/nginx.lock \
   --modules-path=/var/www/modules/ \
-  --without-http_rewrite_module \
+  --with-http_realip_module \
   --with-http_ssl_module \
+  --with-http_v2_module \
+  --with-http_geoip_module \
   --with-pcre \
   --add-dynamic-module=modules/nginx_qat_module/ \
   --with-cc-opt="-DNGX_SECURE_MEM -I/usr/local/include/openssl -Wno-error=deprecated-declarations -Wimplicit-fallthrough=0" \
   --with-ld-opt="-Wl,-rpath=/usr/local/lib64 -L/usr/local/lib64" \
   --user=nginx \
   --group=nginx
-make
-make install
+make && make install
 
 # Configure Intel nginx
 apt update ; apt install -y nginx
@@ -139,6 +141,8 @@ sed -i 's|/usr/sbin/nginx|/usr/local/bin/nginx|g' /lib/systemd/system/nginx.serv
 systemctl daemon-reload
 
 # Configure systemd to load custom version nginx
-sed -i 's|include\ /etc/nginx/modules-enabled/\*\.conf\;|load_module\ /var/www/modules/ngx_ssl_engine_qat_module.so\;|g' /etc/nginx/nginx.conf
+sed -i 's|include\ /etc/nginx/modules-enabled/\*\.conf\;|load_module\ /var/www/modules/ngx_ssl_engine_qat_module.so\;|g' /usr/local/share/nginx/conf/nginx.conf
+sed -i '3 a load_module /var/www/modules/ngx_ssl_engine_qat_module.so;' /usr/local/share/nginx/conf/nginx.conf
+
 systemctl restart nginx
 
